fce5f46374edf9f0c25e34a4ce3d008fcc220171
Mohammad Jabir <mohammadj94@gmail.com>
2023-09-23 01:28:18 -0600

    completed WOULD. completed grid map.

---
+++ 31_FountainOfObjects/ConsoleHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31_FountainOfObjects;
+
+public class ConsoleHelper
+{
+
+}
--- 31_FountainOfObjects/Program.cs
+++ 31_FountainOfObjects/Program.cs
@@ -1,10 +1,67 @@
-﻿namespace _31_FountainOfObjects
+﻿namespace _31_FountainOfObjects;
+
+public class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        // Create grid map
+        GridMap gridMap = CreateMap();
+
+        bool gameRunning = true;
+
+        do
         {
-            Console.WriteLine("Hello, World!");
-        }
+            DisplayMap(gridMap);
+
+        } while (gameRunning);
+
+        Console.ReadLine();
     }
+
+    private static void DisplayMap(GridMap gridMap)
+    {
+        
+    }
+
+    private static GridMap CreateMap()
+    {
+        GridMap output = new GridMap();
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                GridSpot spot = new GridSpot
+                {
+                    Row = j,
+                    Column = i,
+                    Status = GridSpotStatus.Empty
+                };
+
+                if (j == 0 && i == 2) spot.Status = GridSpotStatus.Fountain;
+                if (j == 0 && i == 0) spot.Status = GridSpotStatus.Entrance;
+
+                output.Map.Add(spot);
+            }
+        }
+
+        return output;
+    }
+}
+
+public class GridMap
+{
+    public List<GridSpot> Map { get; set; } = new List<GridSpot>();
+}
+
+public class GridSpot
+{
+    public int Row { get; set; }
+    public int Column { get; set; }
+    public GridSpotStatus Status { get; set; } = GridSpotStatus.Empty;
+}
+
+public enum GridSpotStatus
+{
+    Empty, Player, Entrance, Fountain
 }
